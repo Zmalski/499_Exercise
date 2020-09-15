f90937baf1dfb43e63a0fb15f495a52d0fc3b082
Zach <zacharymaludzinski@gmail.com>
2020-09-15 14:39:34 -0700

    Addition complete

--- calculator.py
+++ calculator.py
@@ -1,1 +1,4 @@
 # This is a simple "calculator" class
+
+def add( num1, num2):
+    return num1 + num2
---
+++ simpleTests.py
@@ -0,0 +1,8 @@
+
+#Test Addition
+from calculator.py import add
+result = add(2, 3)
+if(result == 5):
+    print("Addition test passed.")
+else:
+    print("Addition test failed.")
